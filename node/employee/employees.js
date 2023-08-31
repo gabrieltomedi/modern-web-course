6b8c80edc9155af878a438b9485a1b7e92ea1bb6
Gabriel Oliveira Tomedi <gabriel.tomedi1@gmail.com>
2023-08-31 11:28:54 -0300

    [200~
    git commit -m [200~

--- node/employee/employees.js
+++ node/employee/employees.js
@@ -1,7 +1,20 @@
 const url = 'http://files.cod3r.com.br/curso-js/funcionarios.json'
 const axios = require('axios')
 
+const chineses = employee => employee.pais === 'China'
+const women = employee => employee.genero === 'F'
+const lowestSalary = (employee, currentEmployee) => {
+    return employee.salario < currentEmployee.salario ? employee : currentEmployee
+}
+
 axios.get(url).then(response => {
     const employees = response.data
     console.log(employees)
+
+    const employee = employees
+        .filter(chineses)
+        .filter(women)
+        .reduce(lowestSalary)
+
+    console.log(employee)    
 })
