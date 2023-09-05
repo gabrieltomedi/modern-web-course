0fbc331e837ded3a3755d0e3c597f8f5deb75c08
Gabriel Oliveira Tomedi <gabriel.tomedi1@gmail.com>
2023-09-05 19:56:55 -0300

    creating get and post for the api

--- node/project-api/src/server.js
+++ node/project-api/src/server.js
@@ -2,15 +2,34 @@ const PORT = 3003
 
 const express = require('express')
 const app = express()
+const database = require('./database')
 
-app.get('/products', (req, res, next) => {
-    console.log('Middler 1...')
-    next()
+// app.get('/products', (req, res, next) => {
+//     console.log('Middler 1...')
+//     next()
+// })
+
+// app.get('/products', (req, res, next) => {
+//     res.send({name: 'Notebook', price: 2341.21}) // convert to json
+// })
+
+app.get('/products', (req, res) => {
+    res.send(database.getAllProducts()) 
 })
 
-app.get('/products', (req, res, next) => {
-    res.send({name: 'Notebook', price: 2341.21}) // convert to json
+app.get('/products/:id', (req, res) => {
+    res.send(database.getProduct(req.params.id)) 
 })
+
+app.post('/products', (req, res) => {
+    const product = database.saveProduct({
+        name: req.body.name,
+        price: req.body.price
+    })
+
+    res.send(product)
+})
+
 
 app.listen(PORT, () => {
     console.log(`Server is online on port ${PORT}.`)
