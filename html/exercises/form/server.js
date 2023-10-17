0f98f47f38ab81f7f6cde7614a3d337cf5c98d89
Gabriel Oliveira Tomedi <gabriel.tomedi1@gmail.com>
2023-10-17 16:38:52 -0300

    forms part 3

--- html/exercises/form/server.js
+++ html/exercises/form/server.js
@@ -9,4 +9,10 @@ app.post('/users', (req, resp) => {
     resp.send('<h1>User Added</h1>')
 })
 
+app.post('/users/:id', (req, resp) => {
+    console.log(req.params.id)
+    console.log(req.body)
+    resp.send('<h1>User Updated</h1>')
+})
+
 app.listen(3003)
