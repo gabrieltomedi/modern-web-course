19a6c39622b39e57d9781d52952eec3875b99bbe
Gabriel Oliveira Tomedi <gabriel.tomedi1@gmail.com>
2024-03-06 18:15:39 -0300

    components: father and children

--- react/exercicios/src/components/Father.jsx
+++ react/exercicios/src/components/Father.jsx
@@ -1,14 +1,12 @@
 import React from "react";
-import Son from "./Son"
+import { childrenWithProps } from "../utils/utils";
+
 
 export default props => 
     <div>
         <li>{props.name} {props.surname}</li>
         <h2>Children</h2>
         <ul>
-            <Son name="Anita" surname={props.surname} />
-            <Son {...props} /> {/* spread*/}
-            <Son {...props} name="Carla "/>
-            
+            { childrenWithProps(props)}
         </ul>
     </div>
--- react/exercicios/src/index.js
+++ react/exercicios/src/index.js
@@ -2,12 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import Father from './components/Father'
+import Son from "./components/Son"
 
 //import { GoodMorning, GoodNight } from './components/multipleComponents'
 // import Multi from './components/multipleComponents'
 
 ReactDOM.render(
     <div>
-        <Father name="Gabriel" surname="Oliveira" />
+        <Father name="Gabriel" surname="Oliveira">
+            <Son name="Paulo"  />
+            <Son name="Sara" />
+            <Son name="Luiz" surname="Oliveira" />
+        </Father>
     </div>
 , document.getElementById('root'))
