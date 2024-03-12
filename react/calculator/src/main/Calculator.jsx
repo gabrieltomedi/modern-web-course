db7715462e6d89b00e8ea6502d83cb7f9766f5aa
Gabriel Oliveira Tomedi <gabriel.tomedi1@gmail.com>
2024-03-12 20:54:58 -0300

    Implemening logic for the addDigit and clearMemory methods

--- react/calculator/src/main/Calculator.jsx
+++ react/calculator/src/main/Calculator.jsx
@@ -1,20 +1,31 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import './Calculator.css'
 
 import Button from '../components/Button'
 import Display from '../components/Display'
 
+const initialState = {
+    displayValue: '0',
+    clearDisplay: false,
+    operation: null,
+    values: [0, 0],
+    current: 0
+}
+
 export default class Calculator extends Component {
+
+    state = { ...initialState }
+
     constructor(props) {
         super(props)
+
         this.clearMemory = this.clearMemory.bind(this)        
         this.setOperation = this.setOperation.bind(this)
-        this.setOperation = this.setOperation.bind(this)
-        
+        this.addDigit = this.addDigit.bind(this)        
     }
 
     clearMemory() {
-        console.log('clear')
+        this.setState({ ...initialState })
     }
 
     setOperation(operation) {
@@ -22,7 +33,22 @@ export default class Calculator extends Component {
     }
 
     addDigit(n) {
-        console.log(n)
+        if (n === '.' && this.state.displayValue.includes('.')) {
+            return
+        }
+
+        const clearDisplay = this.state.displayValue === '0' || this.state.clearDisplay
+        const currentValue = clearDisplay ? '' : this.state.displayValue
+        const displayValue = currentValue + n
+        this.setState({ displayValue, clearDisplay: false})
+
+        if ( n !== '.' ) {
+            const i = this.state.current
+            const newValue = parseFloat(displayValue)
+            const values = [...this.state.values]
+            values[i] = newValue
+            this.setState({ values })
+        }
     }
 
     render() {
@@ -31,7 +57,7 @@ export default class Calculator extends Component {
 
         return (
             <div className="calculator">
-                <Display value={100000000000} />
+                <Display value={this.state.displayValue} />
                 <Button label="AC" click={this.clearMemory} triple />
                 <Button label="/" click={this.setOperation} operation />
                 <Button label="7" click={this.addDigit} />
