64c7eea74980a972413cdb3f6ce9d23231d8e30d
Gabriel Oliveira Tomedi <gabriel.tomedi1@gmail.com>
2024-03-12 17:42:36 -0300

    customizing tthe buttons in css

--- react/calculator/src/components/Button.jsx
+++ react/calculator/src/components/Button.jsx
@@ -1,5 +1,29 @@
 import React from "react";
 import './Button.css'
 
-export default props => 
-    <button className='button'>{props.label}</button>
+export default props => {
+    let classNames = `
+        button
+        ${props.operation ? 'operation' : ''}
+        ${props.double ? 'double' : ''}
+        ${props.triple ? 'triple' : ''}
+    `
+
+    return (
+        <button onClick={e => props.click && props.click(props.label)} className={classNames}>
+            {props.label}
+        </button>
+
+    )
+} 
+
+
+// export default props => 
+//     <button className={`
+//         button
+//         ${props.operation ? 'operation' : ''}
+//         ${props.double ? 'double' : ''}
+//         ${props.triple ? 'triple' : ''}
+//     `}>
+//         {props.label}
+//     </button>
