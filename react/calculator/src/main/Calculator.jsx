20eb2e2cedab1c4a270e71e79e9071cf9717d920
Gabriel Oliveira Tomedi <gabriel.tomedi1@gmail.com>
2024-03-13 18:56:15 -0300

    implementing the method setOperation

--- react/calculator/src/main/Calculator.jsx
+++ react/calculator/src/main/Calculator.jsx
@@ -29,7 +29,29 @@ export default class Calculator extends Component {
     }
 
     setOperation(operation) {
-        console.log(operation)
+        if (this.state.current === 0) {
+            this.setState({ operation, current: 1, clearDisplay: true })
+        } else {
+            const equals = operation === '='
+            const currentOperation = this.state.operation
+
+            const values = [...this.state.values ]
+            try {
+                values[0] = eval(`${values[0]} ${currentOperation} ${values[1]}`) // change to swtich
+            } catch(e) {
+                values[0] = this.state.values[0]
+            }
+
+            values[1] = 0
+
+            this.setState({
+                displayValue: values[0],
+                operation: equals ? null : operation,
+                current: equals ? 0 : 1,
+                clearDisplay: !equals,
+                values
+            })
+        }
     }
 
     addDigit(n) {
@@ -37,7 +59,8 @@ export default class Calculator extends Component {
             return
         }
 
-        const clearDisplay = this.state.displayValue === '0' || this.state.clearDisplay
+        const clearDisplay = this.state.displayValue === '0' 
+            || this.state.clearDisplay
         const currentValue = clearDisplay ? '' : this.state.displayValue
         const displayValue = currentValue + n
         this.setState({ displayValue, clearDisplay: false})
