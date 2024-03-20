fbf1480d01f04b70b680275b27c2f5e38dce678d
Gabriel Oliveira Tomedi <gabriel.tomedi1@gmail.com>
2024-03-20 18:01:36 -0300

    implementing component Nav and aplying css style

--- react/crud/frontend/src/componets/template/Nav.jsx
+++ react/crud/frontend/src/componets/template/Nav.jsx
@@ -2,6 +2,14 @@ import './Nav.css'
 import React from 'react'
 
 export default props => 
-    <nav className="menu-area">
-        Navegação
-    </nav>
+    <aside className="menu-area">
+        <nav className="menu">
+            {/* refatorar para os links do menu serem um componente nav-iten*/}
+            <a href="#/">
+                <i className="fa fa-home"></i> Início
+            </a>
+            <a href="#/users">
+                <i className='fa fa-users'></i> Usuários
+            </a>
+        </nav>
+    </aside>
