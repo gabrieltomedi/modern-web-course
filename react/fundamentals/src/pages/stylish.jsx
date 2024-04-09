9e09b4f913e460c2e232525588a047c0bbca5540
Gabriel Oliveira Tomedi <gabriel.tomedi1@gmail.com>
2024-04-09 17:07:34 -0300

    creating a css module

---
+++ react/fundamentals/src/pages/stylish.jsx
@@ -0,0 +1,9 @@
+import styles from "../styles/Stylish.module.css"
+
+export default function Stylish() {
+    return (
+        <div className={styles.purple}>
+            <h1>Style using modules</h1>
+        </div>
+    )
+}
