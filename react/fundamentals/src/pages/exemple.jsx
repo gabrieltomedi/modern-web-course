c11da33c8123bf31064868eb150c52ec9597ab34
Gabriel Oliveira Tomedi <gabriel.tomedi1@gmail.com>
2024-04-09 17:23:45 -0300

    using components properties

---
+++ react/fundamentals/src/pages/exemple.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Header from "../components/Header";
+
+export default function Exemple() {
+    return (     
+        <React.Fragment>
+            <Header title="Next.js and React" />
+            <Header title="Learn Next.js in practices"/>
+        </React.Fragment>   
+    )
+}
--- react/fundamentals/src/pages/index.jsx
+++ react/fundamentals/src/pages/index.jsx
@@ -1,7 +1,7 @@
 export default function Home() {
     return (
         <div>
-            <h1>Home</h1>
+            <h1>Fundamentals of Next.js and React</h1>
             <h2>Home page</h2>
         </div>
     )
