45fb93e11f04673bdd8dd75ff911286b114de5b9
Gabriel Oliveira Tomedi <gabriel.tomedi1@gmail.com>
2024-04-10 16:58:54 -0300

    Creating component Layuot

--- react/fundamentals/src/pages/stylish.jsx
+++ react/fundamentals/src/pages/stylish.jsx
@@ -1,11 +1,12 @@
 import styles from "../styles/Stylish.module.css"
-import Link from 'next/link'
+import Layout from "../components/Layout.jsx"
 
 export default function Stylish() {
     return (
-        <div className={styles.purple}>
-            <h1>Style using modules</h1>
-            <Link href="/">Return</Link>
-        </div>
+        <Layout title="Exemple of css modules">            
+            <div className={styles.purple}>
+                <h1>Style using modules</h1>                
+            </div>
+        </Layout>
     )
 }
