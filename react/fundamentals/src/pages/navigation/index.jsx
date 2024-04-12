8e63a9de684c4f4c8d817bb1c3311f3164e131ec
Gabriel Oliveira Tomedi <gabriel.tomedi1@gmail.com>
2024-04-12 16:25:41 -0300

    Simple Navigation

---
+++ react/fundamentals/src/pages/navigation/index.jsx
@@ -0,0 +1,9 @@
+import Layout from '../../components/Layout'
+
+export default function Navigation() {
+    return (
+        <Layout title="Exemple of navigation #1">
+            <h1>Navigation #1</h1>
+        </Layout>
+    )
+}
