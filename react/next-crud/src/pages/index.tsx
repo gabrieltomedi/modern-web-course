eb4c4049ef7b97bb01e489548de8e4dd7689b797
Gabriel Oliveira Tomedi <gabriel.tomedi1@gmail.com>
2024-04-23 17:02:13 -0300

    Creating component Layout and Title

--- react/next-crud/src/pages/index.tsx
+++ react/next-crud/src/pages/index.tsx
@@ -1,12 +1,19 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import Layout from "@/components/Layout";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <div className={``}>
-
+    <div className={`
+      flex justify-center items-center h-screen
+      bg-gradient-to-r from-blue-500 to-purple-500
+      text-white
+    `}>
+      <Layout title="Cadastro Simples">
+        <span>Content</span>
+      </Layout>
     </div>
   );
 }
