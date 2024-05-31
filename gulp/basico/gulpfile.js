61c12e703413d9a729ca419eabffcc9b75caab9d
Gabriel Oliveira Tomedi <gabriel.tomedi1@gmail.com>
2024-05-31 17:12:16 -0300

    gulp fundamentals #2

--- gulp/basico/gulpfile.js
+++ gulp/basico/gulpfile.js
@@ -1,10 +1,32 @@
 const gulp = require('gulp')
-//const { series } = require('gulp')
-const series = gulp.series
+const { series, parallel } = require('gulp')
+// const series = gulp.series
 
-function copy(callback) {
-    console.log('Tarefa de copiar')
+const before1 = callback => {
+    console.log('Antes 1')
     return callback()
 }
 
-module.exports.default = series(copy)
+const before2 = callback => {
+    console.log('Antes 2')
+    return callback()
+}
+
+function copy(callback) {
+    // gulp.src(['pastaA/arquivo1.txt', 'pastaA/arquivo2.txt'])
+        // .pipe(cortarPelaMetade())
+    gulp.src('pastaA/**/*.txt')
+        .pipe(gulp.dest('pastaB'))
+    return callback()
+}
+
+const end = callback => {
+    console.log('Fim')
+    return callback()
+}
+
+module.exports.default = series(
+    parallel(before1, before2),
+    copy,
+    end
+)
