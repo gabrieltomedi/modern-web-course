4454cbc3458ef0ccee99f2d4b184bf5df3ecb2ed
Gabriel Oliveira Tomedi <gabriel.tomedi1@gmail.com>
2024-08-07 18:03:02 -0300

    configuring output path

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -4,5 +4,9 @@ module.exports = {
     mode: 'development',
     entry: {
         main: './src/main1.js'
+    },
+    output: {
+        filename: 'main.js',
+        path: __dirname + '/public'
     }
 }
