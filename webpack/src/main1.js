87a069c5875456a0ffa962637c02390a8e9b7bec
Gabriel Oliveira Tomedi <gabriel.tomedi1@gmail.com>
2024-08-09 18:02:30 -0300

    loading CSS files

--- webpack/src/main1.js
+++ webpack/src/main1.js
@@ -1,5 +1,6 @@
 import Person from "./person";
 import './modules/moduleA'
+import './assets/css/style.css'
 
 const attendant = new Person
 console.log(attendant.greet())
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -8,5 +8,14 @@ module.exports = {
     output: {
         filename: 'main.js',
         path: __dirname + '/public'
+    },
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: [
+                'style-loader', // add CSS to the DOM by injecting the Tag <style>
+                'css-loader' // interprets @import, url()...
+            ]
+        }]
     }
 }
