b152f2b208bb342973c93202f520797c6eee9bcc
Gabriel Oliveira Tomedi <gabriel.tomedi1@gmail.com>
2024-08-15 17:22:56 -0300

    loading imgs with webpack

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -38,6 +38,11 @@ module.exports = {
                 'css-loader', // interprets @import, url()...
                 'sass-loader'
             ]
-        }]
+        }
+        , {
+            test: /\.(png|jpg|svg|gif)$/ //,
+            //use: ['file-loader']
+        }
+    ]
     }
 }
