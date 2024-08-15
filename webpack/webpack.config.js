1605293c4881b17bd0c52d841dab3440d6c378bd
Gabriel Oliveira Tomedi <gabriel.tomedi1@gmail.com>
2024-08-15 15:05:03 -0300

    setting config for development and production mode

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,15 +1,28 @@
+const modoDev = process.env.NODE_ENV !== 'production'
 const webpack = require('webpack')
-const miniCssExtractPlugin = require('mini-css-extract-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
+const CssMinimizePlugin = require('css-minimizer-webpack-plugin')
 
 module.exports = {
-    mode: 'development',
+    mode: modoDev ? 'development' : 'production',
     entry: {
         main: './src/main1.js'
     },
     output: {
         filename: 'main.js',
         path: __dirname + '/public'
+    },
+    optimization: {
+        minimizer: [            
+            new TerserPlugin({
+                parallel: true,
+                terserOptions: {
+                    ecma: 6,
+                },
+            }), 
+            new CssMinimizePlugin()
+        ]
     },
     plugins: [
         new MiniCssExtractPlugin({
@@ -20,7 +33,7 @@ module.exports = {
         rules: [{
             test: /\.s?[ac]ss$/,
             use: [
-                miniCssExtractPlugin.loader,
+                MiniCssExtractPlugin.loader,
                 // 'style-loader', // add CSS to the DOM by injecting the Tag <style> ^ conflicts with the plugin above
                 'css-loader', // interprets @import, url()...
                 'sass-loader'
